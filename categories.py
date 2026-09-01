33fb1ee5521a07ddfbfe1507690e728f8f94ed3a
Pritee Joshi <pritee814319@gmail.com>
2026-09-01 13:12:36 -0400

    Update categories.py

--- categories.py
+++ categories.py
@@ -1,1 +1,39 @@
 
+CATEGORIES = {
+    "🔥 Trending": {
+        "query": "trending popular videos",
+        "category_id": "0",
+    },
+    "🍔 Food": {
+        "query": "food recipes cooking",
+        "category_id": "0",
+    },
+    "💃 Dance": {
+        "query": "dance",
+        "category_id": "0",
+    },
+    "❤️ Health": {
+        "query": "health fitness wellness",
+        "category_id": "0",
+    },
+    "🎵 Music": {
+        "query": "music",
+        "category_id": "10",
+    },
+    "🎨 Art": {
+        "query": "art",
+        "category_id": "0",
+    },
+    "🖼️ Paintings": {
+        "query": "painting drawing",
+        "category_id": "0",
+    },
+    "👧 Kids": {
+        "query": "kids family",
+        "category_id": "0",
+    },
+    "📰 News": {
+        "query": "news",
+        "category_id": "25",
+    },
+}
